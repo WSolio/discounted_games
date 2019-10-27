340b212b6921125409738a2e4dd6912f2d531f5b
WSolio <54740737+WSolio@users.noreply.github.com>
2019-10-28 01:55:52 +0900

    #우선 origin price가 존재하는 영역의 제목, 제작사, 판매가격을 리스트에 넣어야 하는데. #그 방법을 모르겠음....

--- crawler_text.py
+++ crawler_text.py
@@ -10,24 +10,65 @@ for i in range(4):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(4)
 
-game_detail = driver.find_elements_by_class_name('Vpfmgd')
-top_game_details = driver.find_elements_by_class_name('bQVA0c')
-price_list = driver.find_elements_by_class_name('zYPPle')
-discounted_price_list = driver.find_elements_by_class_name('SUZt4c djCuy')
+#game_titles = driver.find_elements_by_class_name('WsMG1c') ###게임 제목
+#game_publishers = driver.find_elements_by_class_name('KoLSrc') ###게임 제작사
+game_origin_prices = driver.find_elements_by_class_name('SUZt4c') ###게임 원래 가격 <---이 값이 존재하는 게임은 할인중인 게임
+game_saling_prices = driver.find_elements_by_class_name('VfPpfd') ###게임 세일 가격 <---할인되거나, 일반적인 가격
+game_prices = driver.find_elements_by_class_name('LCATme') ###게임 종합 가격 <---둘 다 포함되는 영역
+
+
+#T75of QNCnCf
+#fcxH9b > div.WpDbMd > c-wiz > div > c-wiz > div > c-wiz > c-wiz > c-wiz > div > div.ZmHEEd > div:nth-child(1) > c-wiz > div > div > div.uzcko > div > span.yNWQ8e.K3IMke.buPxGf > span > img
+game_detail = driver.find_elements_by_class_name('VfPpfd')
+
+print(game_detail)
 
 games = []
+#titles = []
+#prices = []
 
-for game in game_detail:
-    a=game.text
-    b=game.src
-    games.append(a)
-    if '0₩' in a:
-        print(num, a)
+games=[]
+
+#우선 origin price가 존재하는 영역의 제목, 제작사, 판매가격을 리스트에 넣어야 하는데.
+#그 방법을 모르겠음....
 
 
-#for price in discounted_price_list:
-#    b = price.text
-#    print(b)
-#    num = num+1
+num = 1
+for game_price in game_prices:
+    a = game_price.text
+for game_origin_price in game_origin_prices:
+    b = game_origin_price.text
+for game_saling_price in game_saling_prices:
+    c = game_saling_price.text
+
+if a == b + c:
+    games.append(b)
+    games.append(c)
+    print(games)
+
+
+
+
+#num = 1
+#for game in game_detail:
+#    a = game.text
+#    games.append(a)
+#    print(num, a)
+#    num = num + 1
+
+
+
+################### 제작사 한개만 리스팅 ####################
+#num = 1
+#for game in game_detail:
+#    a = game.text
+#    if num % 2 == 1:
+#        games.append(a)
+#        print(round(num * 0.5), a)
+#    num = num + 1
+
+
+
+
 
 driver.quit()
